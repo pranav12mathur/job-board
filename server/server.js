d8ef6353240caec0758c15e8afe11a908e892109
pranav12mathur <pranav12mathur@gmail.com>
2026-04-14 11:51:03 +0530

    fix cors for production

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173","https://job-board-xyz.vercel.app"], credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://job-board-api-sigma.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
